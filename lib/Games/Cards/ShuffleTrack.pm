ca86a584aa0521e018ccdf220ad2ed8c115ba5b1
José Castro <cog@cpan.org>
2016-03-02 11:50:18 +0000

    removed package variables

--- lib/Games/Cards/ShuffleTrack.pm
+++ lib/Games/Cards/ShuffleTrack.pm
@@ -20,14 +20,14 @@ Version 0.01_3
 
 our $VERSION = '0.01_3';
 
-our $cut_limits = {
+my $cut_limits = {
 	normal  => [0.19, 0.82], # on a 52 cards deck, cut between 10 and 43 cards
 	short   => [0.09, 0.28], # on a 52 cards deck, cut between 5  and 15 cards
 	center  => [0.36, 0.59], # on a 52 cards deck, cut between 19 and 31 cards
 	deep    => [0.67, 0.86], # on a 52 cards deck, cut between 35 and 45 cards
 };
 
-our $decks = {
+my $decks = {
 	new_deck_order => [qw/AH 2H 3H 4H 5H 6H 7H 8H 9H 10H JH QH KH
 						  AC 2C 3C 4C 5C 6C 7C 8C 9C 10C JC QC KC
 						  KD QD JD 10D 9D 8D 7D 6D 5D 4D 3D 2D AD
