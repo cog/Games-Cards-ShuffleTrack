939ccbf356e6a61037ff5d038da00ae9c46e0d07
José Castro <cog@cpan.org>
2016-03-14 13:08:18 +0000

    incresed the version number

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Games::Cards::ShuffleTrack - Track cards through shuffles and cuts
 
 # VERSION
 
-Version 0.03\_2
+Version 0.03\_3
 
 # SYNOPSIS
 
--- lib/Games/Cards/ShuffleTrack.pm
+++ lib/Games/Cards/ShuffleTrack.pm
@@ -14,11 +14,11 @@ Games::Cards::ShuffleTrack - Track cards through shuffles and cuts
 
 =head1 VERSION
 
-Version 0.03_2
+Version 0.03_3
 
 =cut
 
-our $VERSION = '0.03_2';
+our $VERSION = '0.03_3';
 
 my $cut_limits = {
 	normal  => [0.19, 0.82], # on a 52 cards deck, cut between 10 and 43 cards
