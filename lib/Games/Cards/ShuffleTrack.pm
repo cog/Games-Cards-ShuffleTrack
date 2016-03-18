d3fb3fb657c91c0ab512d801b89ae8e4330c7979
Jose Castro <cog@cpan.org>
2016-03-18 00:25:56 +0000

    moved a note to a different file

--- lib/Games/Cards/ShuffleTrack.pm
+++ lib/Games/Cards/ShuffleTrack.pm
@@ -544,6 +544,7 @@ You can also decide where to cut the deck for the shuffle:
 # TODO: add an option to control top or bottom stock
 # TODO: when dropping cards, should we favor numbers 2 and 3?
 # TODO: with a lot of cards, the riffle should be done breaking the deck in piles
+# TODO: consider how fast each half is being depleted and whether the packets riffled on each side are of similar sizes
 sub riffle_shuffle {
 	my $self  = shift;
 	my $depth = shift;
